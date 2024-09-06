3ad01291ee90a412263a6cedadbff8412f6e2cac
Cesarw-Vidal <cesar.vidalmnd@uanl.edu.mx>
2024-09-05 19:22:57 -0600

    MicroServicios Spring Boot 1/2 CDVM

---
+++ spring-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S21', now());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S22', now());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Samsung', 'S23', now());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'IPhone 14', now());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Apple', 'IPhone 15', now());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Xiaomi', 'Mi', now());
